1796dfc702619439dbd90f7a3edb47e1404f9c62
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2023-03-18 19:51:59 +0000

    adding services section

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Backend = () => {
   return (
     <div className="skills__content">
-        <h3 className="skills__title"></h3>
+        <h3 className="skills__title">Backend Developer</h3>
 
         <div className="skills__box">
             <div className="skills__group">
@@ -12,8 +12,8 @@ const Backend = () => {
                 <i class='bx bx-badge-check' ></i>
 
                 <div>
-                    <h3 className="skills__name"></h3>
-                    <span className="skills__level"></span>
+                    <h3 className="skills__name">Node.js</h3>
+                    <span className="skills__level">Basic</span>
                 </div>
                 </div>
 
@@ -21,8 +21,8 @@ const Backend = () => {
                 <i class='bx bx-badge-check' ></i>
 
                 <div>
-                    <h3 className="skills__name"></h3>
-                    <span className="skills__level"></span>
+                    <h3 className="skills__name">Python</h3>
+                    <span className="skills__level">Intermediate</span>
                 </div>
                 </div>
 
@@ -30,8 +30,8 @@ const Backend = () => {
                 <i class='bx bx-badge-check' ></i>
 
                 <div>
-                    <h3 className="skills__name"></h3>
-                    <span className="skills__level"></span>
+                    <h3 className="skills__name">Ruby</h3>
+                    <span className="skills__level">Basic</span>
                 </div>
                 </div>
 
@@ -39,8 +39,41 @@ const Backend = () => {
                 <i class='bx bx-badge-check' ></i>
 
                 <div>
-                    <h3 className="skills__name"></h3>
-                    <span className="skills__level"></span>
+                    <h3 className="skills__name">MySQL</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check' ></i>
+
+                <div>
+                    <h3 className="skills__name">Firebase</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+                <div className="skills__data">
+                <i class='bx bx-badge-check' ></i>
+
+                <div>
+                    <h3 className="skills__name">SqlLite</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+                <div className="skills__data">
+                <i class='bx bx-badge-check' ></i>
+
+                <div>
+                    <h3 className="skills__name">Rust</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+                <div className="skills__data">
+                <i class='bx bx-badge-check' ></i>
+
+                <div>
+                    <h3 className="skills__name">Go</h3>
+                    <span className="skills__level">Intermediate</span>
                 </div>
                 </div>
             </div>
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./skills.css";
 import Backend from './Backend';
 import Frontend from './Frontend';
 
 const Skills = () => {
   return (
     <section className='skills section' id="skills">
-            <h2 className="section__title">Skills</h2>
-       <span className="section__subtitle">My Tecchnical Level</span>
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My Tecchnical Level</span>
 
        <div className="skills__container container grid">
         <Frontend />
         <Backend />
 
        </div>
-    </section>
-  )
+    </section>                                  
+  );
 };
 
 export default Skills
